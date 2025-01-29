775570343154f98a900cccc6ac5e0ebf6b46fd09
Juan Chuc <dev.juan.eloper@gmail.com>
2025-01-28 22:35:36 -0600

    initial create token

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,6 +1,11 @@
 use actix_web::{web::Json, HttpResponse};
+use jsonwebtoken::{Algorithm, Header};
 
 use crate::schema::user;
+
+async fn create_token(time_expiration_min: i64, user: User) -> String {
+    let header = Header::new(Algorithm::HS512);
+}
 
 async fn get_token_header(Json(user): Json<User>) -> HttpResponse {
     let token = jwt_lib::get_jwt(user);
